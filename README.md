bb6180e15cd593ff41f6a9fe2b1daea40373c966
Sandra Misigo <misigosandra@gmail.com>
2024-12-19 02:03:38 +0300

    Brewery-HUB

--- README.md
+++ README.md
@@ -1,1 +1,99 @@
-# Brewery-HUB
+ # The Brewery Hub
+ Welcome to The Brewery Hub! An online Single Page Application that allows users to search a brewery by name, view individual brewery details, filter different brewerys by type as well as add a brewery to a Favorite List. The application is designed using JavaScript, HTML and CSS and utilizes The Open Brewery DB API.
+
+ Here is a live link to The Brewery Hub application:
+
+
+ ## Project Learning Goals
+   1. To design and architect features across a frontend.
+   2. To communicate and collaborate in a technical environment.
+   3. To intergrate Javascript to an external API.
+   4. To debug issues in small to medium sized projects.
+   5. To build and iterate on a project MVP.
+
+
+ ## Project Requirements
+   1. A single Page Application with a HTML,JavaScipt and CSS frontend.
+   2. The app should access data from a public API. 
+   3. Have 3 distinct event listener as;
+           * Search
+           * Filter
+           * Add 
+   4. Implement at least one Array Iteration Method such as:
+           * Map
+           * forEach
+           * filter.
+   5. Follow good coding practices. Keeping the code DRY.
+
+
+ ## Project Setup
+
+  1. Creating Project Folder: 
+  2. Initializing a Git repository to track changes.
+  3. Regularly commiting code to the repository to maintain a clean history and facilitate backups.
+
+
+ ## Core Deliverables
+
+  1. **Search for a Brewery by Name** 
+    - Users can type in the search bar the name of a specific brewery. The app then fetches matching data from the OPEN BREWERY DB API and displays it on the web page.
+  2. **Filtering Brewerys**
+    - Users can filter through brewerys by type.All they have to do is click a drop down menu and the brewery types will be displayed such as: nano, proprietor,micro,contract and brewpub. Selecting one means only the brewery type with matching data will be displayed.
+  3. **Viewing Brewery Details**
+    - Clicking a single brewery on the list enables one to view its details such as:
+      * type
+      * address
+      * contact
+      * website     
+  4. **Adding Brewery to a Favorite List**
+    - After scrolling through a list of brewerys, a user can add their favorite brewery to a Favorites List. In case they want to visit the brewery, they can always refer to their Favorite List.
+  5. **Load Brewerys** 
+    - This allows users to view all the brewerys on the web page.
+
+
+  ## Project Structure
+      1. **OPEN BREWERY DB**: Provides the brewery data used in the app.
+      2. **index.html**: Contains the basic HTML structure and layout of the web page.
+      3. **style.css**: This ensures basic styling is done to the application layout such as font size, color etc.
+      4. **index.js**: This is where DOM manipulation, event handling, and server communications are handled.
+    
+
+  ## Getting Started
+
+  ## Setup Instructions
+
+      1. **Clone the Repository:**
+
+       * git clone [https://github.com/sandramisigo/Flatdango](https://github.com/sandramisigo/Flatdango)
+       * cd Flatdango
+
+      2. **Navigate to the project directory**
+
+      3. **Open the Application:**
+
+       * Open the `index.html` in your browser to view and interact with the app.
+
+  ## **Conclusion**
+
+    This project provides a simple and yet effective way to interact with your best brewery. You search a brewery and easily add it to a 'Favorites List' in order to reach out in future. The project is ideal for practicing basic front-end development skills, including DOM manipulation, event handling among others.
+
+  ## **License**
+
+    This project is open-source and free to use under the MIT License. Feel free to modify and share as needed.
+
+  **Author:** 
+    Sandra Misigo
+
+  **Contact:**
+    For any questions or incase you experience challenges, feel free to contact me via [sandra.misigo@student.moringaschool.com]. Good luck!
+
+
+
+
+
+
+
+
+
+
+
--- index.js
+++ index.js
@@ -1,1 +1,145 @@
+document.addEventListener('DOMContentLoaded', () => {
 
+//Referencing
+
+const loadBrewerysButton = document.getElementById('load-brewerys');
+const breweryTypeSelector = document.getElementById('brewery-type'); 
+const brewerySearchInput = document.getElementById('brewery-search');
+const breweryListSection = document.getElementById('brewerys');
+const breweryList = document.getElementById('brewery-list');
+const breweryDetailSection = document.getElementById('brewery-details');
+const breweryInformation = document.getElementById('brewery-information');
+const favoritesListSection = document.getElementById('favorites-list');
+const favoritesListDisplay = document.getElementById('favorites-list-display');
+const viewFavoritesButton= document.getElementById('view-favorites-button');
+
+const favorites = [];
+
+//Adding the event listeners
+brewerySearchInput.addEventListener('input', searchbrewerys);
+breweryTypeSelector.addEventListener('change',filterByType);
+loadBrewerysButton.addEventListener('click',loadBrewerys);
+viewFavoritesButton.addEventListener('click', displayFavorites);
+
+//Fetch brewerys from OPEN BREWERY DB API
+function loadBrewerys() {
+    fetch(`https://api.openbrewerydb.org/breweries`)
+    .then(response => response.json())
+    .then(data =>displayBrewerys(data))
+    .catch(error => console.error('Encountered error while fetching brewerys:',error));
+}
+
+// displaying the brewerys
+function displayBrewerys(brewerys) {
+    breweryList.innerHTML='';
+    brewerys.forEach(brewery => {
+        const li = document.createElement('li');
+        li.textContent = brewery.name;
+
+//Adding a Favorites button
+const favoritesButton = document.createElement('button');
+favoritesButton.textContent = 'Add to favorites';
+favoritesButton.addEventListener('click', () => addToFavorites(brewery));
+
+li.appendChild(favoritesButton);
+breweryList.appendChild(li);
+
+li.addEventListener('click',() => loadBreweryDetails(brewery.id));
+ });
+ breweryListSection.classList.remove('hidden');
+}
+
+//function to add brewery to favorites
+function addToFavorites(brewery){
+    if(!favorites.includes(brewery))
+        favorites.push(brewery);
+        updateFavorites();
+}
+
+//Function to update favorites display
+function updateFavorites() {
+    favoritesListDisplay.innerHTML ='';
+    
+    favorites.forEach((brewery,index) => {
+       const li = document.createElement('li');
+       li.textContent = brewery.name;
+
+ //adding a "remove from favorites" button
+const removeButton = document.createElement('button');
+removeButton.textContent="Remove item";
+removeButton.addEventListener('click',() => removeFromFavorites(brewery));      
+   
+li.appendChild(removeButton);
+favoritesListDisplay.appendChild(li);
+});
+
+//display view favorites if there are favorites
+if(favorites.length > 0) {
+    viewFavoritesButton.classList.remove('hidden');
+} else {
+    viewFavoritesButton.classList.add('hidden');
+}
+}
+
+//function to remove brewery from favorites
+function removeFromFavorites(breweryToRemove) {
+    const index = favorites.indexOf(breweryToRemove);
+    if (index> -1){
+        favorites.splice(index,1);   
+    }
+    updateFavorites();
+} 
+//function to display favorites
+function displayFavorites (){
+    alert('Thank you for Favoriting these Brewerys! You can go ahead and Plan your Visit Now!');
+    favoritesListDisplay.length = 0;
+    updateFavorites();
+}
+
+//fetching and displaying brewery details
+function loadBreweryDetails(breweryId) {
+    fetch(`https://api.openbrewerydb.org/breweries/${breweryId}`)
+    .then(response => response.json())
+    .then(brewery => {
+      breweryInformation.innerHTML = `
+        <h3>${brewery.brewery_type}</h3>
+        <p>Type:${brewery.brewery_type}</p>
+        <p>Adress:${brewery.address_1}, ${brewery.city}, ${brewery.state} , ${brewery.country}</p>
+        <p>Phone:${brewery.phone}</p>
+        <p>Website: <a href="${brewery.website_url}" target="blank">${brewery.website_url}</a></p>
+        `;
+      breweryDetailSection.classList.remove('hidden');
+    })
+    .catch(error => console.error(`Encountered error fetching brewery details:`,error));
+}
+
+//function to filter brewery by type
+function filterByType(){
+    const breweryType = breweryTypeSelector.value;
+     let endpoint = `https://api.openbrewerydb.org/breweries`;
+
+    if(breweryType && breweryType !== 'All'){
+        endpoint +=`?by_type=${breweryType}`;
+    }
+
+    fetch(endpoint)
+    .then(response => response.json())
+    .then(data => displayBrewerys(data))
+    .catch(error => console.error(`Encountered error fetching filtered brewerys:`, error));
+}
+
+//function to search brewerys by name
+function searchbrewerys() {
+    const searchWord= brewerySearchInput.value;
+
+    fetch(`https://api.openbrewerydb.org/breweries`)
+    .then(response=> response.json())
+    .then(brewerys => {
+        const filteredbrewerys = brewerys.filter(brewery => 
+            brewery.name.includes(searchWord)
+        );
+        displayBrewerys(filteredbrewerys);
+    })
+    .catch(error => console.error(`Encountered error searching:`, error));
+}
+});
